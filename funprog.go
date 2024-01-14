7a65e449200d7a900bb005e1dd4c18fbd34d2daa
wipdev-tech <wipdev.tech@gmail.com>
2024-01-14 09:29:11 +0200

    Added func Filter with tests

--- funprog.go
+++ funprog.go
@@ -10,3 +10,17 @@ func Map[T any](f func(T) T, s []T) []T {
 	}
 	return out
 }
+
+// func Filter implements the common high-order function `filter`. It takes a
+// function (predicate) and a slice of some type, and it returns a slice
+// containing the elements of the input slice for which the predicate returns
+// true.
+func Filter[T any](p func(T) bool, s []T) []T {
+	out := make([]T, 0, len(s))
+	for _, el := range s {
+		if p(el) {
+			out = append(out, el)
+		}
+	}
+	return out
+}
--- funprog_test.go
+++ funprog_test.go
@@ -28,3 +28,27 @@ func TestMap(t *testing.T) {
 		t.Fatalf("Expected %v, got %v", e2, r2)
 	}
 }
+
+func TestFilter(t *testing.T) {
+	s1 := []int32{1, 2, 3, 5}
+	f1 := func(x int32) bool {
+		return x%2 == 0
+	}
+	e1 := []int32{2}
+	r1 := funprog.Filter(f1, s1)
+
+	if !slices.Equal(r1, e1) {
+		t.Fatalf("Expected %v, got %v", e1, r1)
+	}
+
+	s2 := []string{"hello", "world", "hi"}
+	f2 := func(s string) bool {
+		return strings.HasPrefix(s, "h")
+	}
+	e2 := []string{"hello", "hi"}
+	r2 := funprog.Filter(f2, s2)
+
+	if !slices.Equal(r2, e2) {
+		t.Fatalf("Expected %v, got %v", e2, r2)
+	}
+}
